cfa3454a02ebcdbc827ba85497d223852930036d
Jared Bacik <jb@jaredbacik.com>
2024-07-08 14:21:42 -0400

    initial code

---
+++ src/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hey welcome to checkout, what was the subtotal?");
+
+var subtotal = Console.ReadLine();
+
+Console.WriteLine("Calculating covered costs...");
+var costs = GetDonorCoveredCosts(subtotal);
+
+Console.WriteLine($"The donor covered costs are {costs}");
+
+var total = subtotal + costs;
+Console.WriteLine($"Thanks your total is 👋");
+
+object GetDonorCoveredCosts(string? subtotal)
+{
+    throw new NotImplementedException();
+}
